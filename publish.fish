24449bae0c17a9506384ef5384b2f33af04802fd
Fe3O4-Git <Fe3O4_Live@Outlook.com>
2022-01-23 19:57:31 +0800

    test publish.fish with space

--- publish.fish
+++ publish.fish
@@ -2,10 +2,10 @@
 
 # publish source
 git add .
-git commit -m \'$argv\'
-git push origin master
+git commit -m "$argv"
+#git push origin master
 hugo --minify
 cd public
 git add .
-git commit -m \'$argv\'
-git push origin master
+git commit -m "$argv"
+#git push origin master
